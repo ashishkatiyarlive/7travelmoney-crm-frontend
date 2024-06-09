893a9c11646a4169fbe7b55fca9d6489e8e1675e
ashishkatiyarlive <ashish.live@yahoo.com>
2024-06-09 12:47:13 +0530

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+//import Validation from './utils/validation';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  // loginForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+  // ngOnInit(){
+  //   this.loginForm = this.formBuilder.group({
+  //     email: ['', [Validators.required, Validators.email]],
+  //     password: ['', Validators.required]
+  //   });
+  // }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // console.log('Form Submitted!');
+      // console.log('Email:', this.loginForm.value.email);
+      // console.log('Password:', this.loginForm.value.password);
+      // You can add your authentication logic here
+    } else {
+      console.error('Form is invalid');
+    }
+  }
+
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  touched = false;
+
+  ngOnInit(): void{
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+  get f(): { [key: string]: AbstractControl } {
+    return this.loginForm.controls;
+  }
+}
