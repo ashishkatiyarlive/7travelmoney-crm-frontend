13ee58d72c569497b42184851972ddca7e20a68c
Kedar nath Singh <nathkd@gmail.com>
2024-06-16 12:29:09 +0530

    order listing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { UserDashboardComponent } from './user-dashboard/user-dashboard.component';
+import { UserListingComponent } from './user-listing/user-listing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent, pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'user-dashboard', component: UserDashboardComponent},
+  {path:'user-listing', component: UserListingComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
