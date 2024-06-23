07c112fd2db13ba3f1b27031b893ad575ae004ee
Kedar nath Singh <nathkd@gmail.com>
2024-06-23 17:24:56 +0530

    add User

--- src/app/add-user/add-user.component.ts
+++ src/app/add-user/add-user.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { Password } from 'primeng/password';
 
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrl: './add-user.component.css'
 })
-export class AddUserComponent {
+export class AddUserComponent implements OnInit {
+  addUser!: FormGroup;
+  submitted = false;
+  constructor( private fb: FormBuilder){
+  }
+
+  ngOnInit(){
+    this.addUser = this.fb.group({
+      userName: ['', Validators.required],
+      password: ['', Validators.required],
+      mobile: ['', Validators.required],
+      email: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required], 
+      address: ['', Validators.required]
+    })
+  }
+
+  get userData(){
+    return this.addUser.controls;
+  }
+
+  submitUserForm(){
+    this.submitted = true;
+    console.log(this.addUser.value)
+
+  }
 
 }
